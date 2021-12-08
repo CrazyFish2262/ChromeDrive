8309a3703198b21462665ea5919af8e322aa69b6
st <12>
2021-12-08 21:10:26 +0300

    Автоматизация

--- src/test/java/TestAddRemove.java
+++ src/test/java/TestAddRemove.java
@@ -1,14 +1,19 @@
+import com.sun.javafx.scene.control.behavior.SliderBehavior;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import sun.reflect.misc.FieldUtil;
 
+import java.io.File;
+import java.security.SecureRandom;
 import java.util.List;
 
 public class TestAddRemove {
     WebDriver driver;
+    SecureRandom random = new SecureRandom();
 
     @BeforeTest
     public void setUp() {
@@ -17,9 +22,9 @@ public class TestAddRemove {
         driver.manage().window().maximize();
 
     }
-/*
+    /*
 
-*/
+     */
 /*
     @Test
     public void findElement() {
@@ -74,7 +79,7 @@ public void listE() {
 */
 
 
-@Test
+/*@Test
     public void findMers(){
     driver.get("https://www.onliner.by/");
     WebElement autobaraholka = driver.findElement(By.xpath(".//ul[@class='b-main-navigation']/li[3]"));
@@ -88,11 +93,63 @@ public void listE() {
     });
 
 
-    //*[@id="container"]/div/div/div/div/div/div[2]/div/div/div[3]/div/div[2]/div[2]/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div[1]/div[2]
+}*/
+
+
+/*
+@Test
+    public void inputPlaceholder(){
+    driver.get("https://www.onliner.by");
+    WebElement placeholder = driver.findElement(By.className("fast-search__input"));
+    String actualResult = placeholder.getAttribute("placeholder");
+    System.out.println(actualResult);
+Assert.assertTrue(actualResult.contains("Поиск в Каталоге."));
+
+}
+*/
+public long getFolderSizeBefore() {
+    String folderPath = "C:\\Users\\st\\Downloads\\";
+    File file = new File(folderPath);
+    long length = 0;
+    for (int i = 0; i < file.list().length; i++) {
+        File tmp = new File(folderPath + file.list()[i]);
+        length += tmp.length();
+    }
+    return length;
 }
 
 
+    @Test
+    public void downloadFiles() throws InterruptedException {
+        Long sizeBefore = getFolderSizeBefore();
+    System.out.println("Folder size before downloading: " + sizeBefore + " bytes");
+        driver.get("https://the-internet.herokuapp.com/download");
+        List<WebElement> list = driver.findElements(By.xpath("//div[@id=\"content\"]/div//a"));
+/*        list.forEach(element ->
+                System.out.println(element.getAttribute("href")));*/
+        WebElement file = list.get(random.nextInt(list.size()));
+        file.click();
+        Thread.sleep(5000);
+        System.out.println();
+        System.out.println("Downloaded file is " + file.getText());
+        String folderPath = "C:\\Users\\st\\Downloads\\";
+        File file1 = new File(folderPath + file.getText());
+        System.out.println("It's size is " + file1.length() + " bytes");
+        System.out.println("Folder size after downloading: " + getFolderSizeAfter() + " bytes");
+        Long sizeAfter = getFolderSizeAfter();
+        Assert.assertEquals(sizeAfter + 0, sizeBefore + file1.length());
+    }
 
 
 
+    public long getFolderSizeAfter() {
+        String folderPath = "C:\\Users\\st\\Downloads\\";
+        File file = new File(folderPath);
+        long length = 0;
+        for (int i = 0; i < file.list().length; i++) {
+            File tmp = new File(folderPath + file.list()[i]);
+            length += tmp.length();
+        }
+        return length;
+    }
 }
